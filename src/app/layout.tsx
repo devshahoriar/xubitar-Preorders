dd0923b16eebc991da78dec235b7af7a738946d1
devshahoriar <shuvo1556@gmail.com>
2026-06-23 16:29:45 +0600

    feat: implement root layout with TRPC, theme, and toast providers

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,17 @@
 import { ThemeProvider } from "@/components/providers/theme-provider";
 import Header from "@/components/shared/Header";
+import { Toaster } from "@/components/ui/sonner";
 import { cn } from "@/lib/utils";
 import "@/styles/globals.css";
 import { TRPCReactProvider } from "@/trpc/react";
 import { type Metadata } from "next";
 import { Geist, Inter } from "next/font/google";
 import { NuqsAdapter } from "nuqs/adapters/next/app";
-import { Toaster } from "@/components/ui/sonner"
 
 const inter = Inter({ subsets: ["latin"], variable: "--font-sans" });
 
 export const metadata: Metadata = {
+  metadataBase: new URL("http://localhost:3000/"),
   title: "Preorder Manager",
   description: "Xubitar Preorder Manager",
   icons: [{ rel: "icon", url: "/favicon.ico" }],
